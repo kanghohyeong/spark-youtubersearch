c5f540194d3265e5ae2e48b728c46edfd24ed49a
호형 강 <fourierk@naver.com>
2020-11-13 02:59:44 +0900

    banner css chnage

--- prototype/src/banner.js
+++ prototype/src/banner.js
@@ -1,32 +1,49 @@
 import React, {useState, useEffect, Component} from 'react';
 import { Link } from 'react-router-dom';
+import './banner.css';
 
 
-export default function Banner() {
 
-  const style = {
-    width: "100%",
-    backgroundColor: "#384259",
-    display: "flex",
-    justifyContent: "space-around",
-    padding: 5
+class banner extends Component {
+  render() {
+    return (
+      <div className="top-banner">
+       <h2 className="logo-contain"><Link to="/" className="title-logo">YouReco!</Link></h2>
+       <input className="search-box" type={"text"} placeholder={"유튜버를 검색하세요"}/>
+       <button className="end-button">종료하기</button>
+     </div>
+    );
   }
-
-  const font = {
-    // fontFamily: "luckiest-guy"
-    color: "#F73859",
-    fontSize: 30
-  }
-
-  const textColor = {
-    color: "#FFF"
-  }
-
-  return(
-    <div style={style}>
-      <h2><Link to="/" style={font}>Youreco!</Link></h2>
-      <input type={"text"} placeholder={"유튜버를 검색하세요"}/>
-      <h2 style={textColor}>로그인 회원가입</h2>
-    </div>
-  )
 }
+
+export default banner;
+
+
+// export default function Banner() {
+
+//   const style = {
+//     width: "100%",
+//     backgroundColor: "#384259",
+//     display: "flex",
+//     justifyContent: "space-around",
+//     padding: 5
+//   }
+
+//   const font = {
+//     // fontFamily: "luckiest-guy"
+//     color: "#F73859",
+//     fontSize: 30
+//   }
+
+//   const textColor = {
+//     color: "#FFF"
+//   }
+
+//   return(
+//     <div style={style}>
+//       <h2><Link to="/" style={font}>YouReco!</Link></h2>
+//       <input type={"text"} placeholder={"유튜버를 검색하세요"}/>
+//       <h2 style={textColor}>로그인 회원가입</h2>
+//     </div>
+//   )
+// }
