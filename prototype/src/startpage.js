eab90d020639822908e05896419c691f290ab6da
호형 강 <fourierk@naver.com>
2020-11-14 02:02:38 +0900

    startpage css

--- prototype/src/startpage.js
+++ prototype/src/startpage.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect, Component} from 'react';
 import ReactModal from 'react-modal'
+import './startpage.css';
+
+
 export default function Start() {
   const list = category.map(it => false)
 
@@ -51,26 +54,34 @@ export default function Start() {
   }
 
   return(
-    <div>
-      <p>당신의 취향에 맞는 유튜버 서비스</p>
-      <h1>YOURECO!</h1>
-      <div>
-        <label for="gender">성별
-          <input type='radio' name='gender' value='male' checked={gender === "남성"} onChange={()=>setGender("남성")}/>남성
-          <input type='radio' name='gender' value='female' checked={gender === "여성"} onChange={()=>setGender("여성")}/>여성
+    <div className="startpage-contain">
+      <div className="top-title">
+        <p>당신의 취향에 맞는 유튜버 서비스</p>
+        <h1>YouReco!</h1>
+      </div>
+      
+      <div className="int-contain">
+        <label className="gender" for="gender"><span>성별</span>
+          <span>
+            <span><input type='radio' name='gender' value='male' checked={gender === "남성"} onChange={()=>setGender("남성")}/>남성</span>
+            <span><input type='radio' name='gender' value='female' checked={gender === "여성"} onChange={()=>setGender("여성")}/>여성</span>
+          </span>
         </label>
         <br/>
-        <label for="usual">평소 유튜브 시청 카테고리
+
+        <label className="cate-box" for="usual"><p>평소 유튜브 시청 카테고리</p>
           {category.map((it, idx) => <div><input type="checkbox" name="usual" value={it} checked={usual[idx]} onChange={() => handleChange(usual, setUsual, idx)}/>{it}</div>)}
           {usual[usual.length - 1]? <input type="text" placeholder="기타에 해당하는 카테고리 입력" value={usualEtc} onChange={(e) => setUsualEtc(e.target.value)}/> : null}
         </label>
         <br/>
-        <label for="new">새로운 유튜버를 추천받고싶은 카테고리
+
+        <label className="cate-box" for="new"><p>새로운 유튜버를 추천받고싶은 카테고리</p>
           {category.map((it, idx) => <div><input type="checkbox" name="new" value={it} checked={want[idx]} onChange={() => handleChange(want, setWant, idx)}/>{it}</div>)}
           {want[want.length - 1]? <input type="text" placeholder="기타에 해당하는 카테고리 입력" value={wantEtc} onChange={(e) => setWantEtc(e.target.value)}/> : null}
           <br/>
         </label>
-        <button type="button" onClick={() => setIsLastPage(true)} disabled={disabled} >시작하기</button>
+
+        <button className="start-button" type="button" onClick={() => setIsLastPage(true)} disabled={disabled} >시작하기</button>
       </div>
 
       <ReactModal isOpen={isLastPage} contentLabel="마지막안내" style={style}>
