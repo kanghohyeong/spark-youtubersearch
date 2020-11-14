db8b9daa28598dd3756e1fe24fc852a843004982
호형 강 <fourierk@naver.com>
2020-11-14 16:40:17 +0900

    새 패이지 갱신 시 scroll top 구현

--- prototype/src/index.js
+++ prototype/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ScrollToTop from './scrollToTop';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
