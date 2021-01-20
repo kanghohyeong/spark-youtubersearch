cf89a071355361ae0c98974a705823a3c46e4a7c
호형 강 <fourierk@naver.com>
2021-01-21 00:47:21 +0900

    유튜브 내 데이터로 키워드 매핑 구현 / mongoDB 접속 테스트

---
+++ crawler/pymongo_test.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient, collection
+from pymongo import cursor
+from pymongo.cursor import CursorType
+
+
+host = 'localhost'
+port = '27017'
+mongo = MongoClient(host, int(port))
+print(mongo.list_database_names())
+
+db = mongo.spark612
+
+collection_ = db.channels
+
+item = collection_.find_one()
+
+print(item)
